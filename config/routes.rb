6340cba184b6631d58bc173640d51ec1c53712fa
morecan <olympspartan@gmail.com>
2020-01-20 16:09:07 +0200

    first steps

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  resources :jobs
+  get 'dashboard/index'
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'application#index'
+  get 'users' => 'users#index', as: :users_path
+  get 'dashboard' => 'dashboard#index', as: :dashboard_path
 end
